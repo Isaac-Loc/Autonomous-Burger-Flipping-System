9e11270a72a1ba3f32e586bd7e6f674ba4ea0fde
Isaac <isaacloc@buffalo.edu>
2025-03-03 12:51:46 -0500

    Servo Spinner Function
    This function provides functionality for general servo use.

--- Burger_Flipper_Code/Burger_Flipper_Code.ino
+++ Burger_Flipper_Code/Burger_Flipper_Code.ino
@@ -1,9 +1,21 @@
 #include <Servo.h>
 
+Servo SpatulaServo;
+Servo ProbeServo;
+
 void setup() {
   // put your setup code here, to run once:
   
 }
+
+void ServoSpinner(int angle, int angle2, Servo servo){
+  servo.write(angle1);
+  delay(1000);
+  servo.write(angle2);  
+  delay(1000);
+}
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
