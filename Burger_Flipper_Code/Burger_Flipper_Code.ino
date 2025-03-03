a6fd737c1cb616f556b37af24b70731e08fbd40d
Isaac <isaacloc@buffalo.edu>
2025-03-03 12:58:21 -0500

    Servo update
    Updated servo function with comments and added pin attachment functions for servos in setup.

--- Burger_Flipper_Code/Burger_Flipper_Code.ino
+++ Burger_Flipper_Code/Burger_Flipper_Code.ino
@@ -5,14 +5,15 @@ Servo ProbeServo;
 
 void setup() {
   // put your setup code here, to run once:
-  
+  SpatulaServo.attach(9); //attach spatula servo to pin 9
+  ProbeServo.attach(10); //attach probe servo to pin 10
 }
 
-void ServoSpinner(int angle, int angle2, Servo servo){
-  servo.write(angle1);
-  delay(1000);
-  servo.write(angle2);  
-  delay(1000);
+void ServoSpinner(int angle1, int angle2, Servo servo){
+  servo.write(angle1); //moves the servo angle1 degrees
+  delay(1000); //1 second delay
+  servo.write(angle2); //moves the servo angle2 degrees  
+  delay(1000); //1 second delay
 }
 
 
