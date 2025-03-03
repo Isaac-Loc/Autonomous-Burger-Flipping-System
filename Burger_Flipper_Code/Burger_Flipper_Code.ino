2a2473caea8fb3b42a10cd99ae0dfda324492a7d
Isaac <isaacloc@buffalo.edu>
2025-03-03 13:33:44 -0500

    Temperature sensor
    adds a function that gets temperature and also includes more comments

--- Burger_Flipper_Code/Burger_Flipper_Code.ino
+++ Burger_Flipper_Code/Burger_Flipper_Code.ino
@@ -1,12 +1,26 @@
 #include <Servo.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-Servo SpatulaServo;
-Servo ProbeServo;
+#define ONE_WIRE_BUS 2; //uses pin 2 for onewire communication
+
+OneWire oneWire(ONE_WIRE_BUS); //sets up onewire communcation
+DallasTemperature sensor(&oneWire); //sets up the DallasTemperature library to work with the OneWire bus
+
+Servo SpatulaServo; //Spatula Servo object
+Servo ProbeServo; //Probe Servo object
 
 void setup() {
   // put your setup code here, to run once:
+  sensor.begin(); //initializes the sensor for arduino communication
   SpatulaServo.attach(9); //attach spatula servo to pin 9
   ProbeServo.attach(10); //attach probe servo to pin 10
+}
+
+float getTemperature(){
+  sensor.requestTemperature(); //requests a temperature reading from the sensor
+  float temperature = sensor.getTempFByIndex(0); //stores the temperature from the sensor
+  return temperature; //returns the temperature value in degrees Fahrenheit
 }
 
 void ServoSpinner(int angle1, int angle2, Servo servo){
