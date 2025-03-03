2f784c5cc4397fadafab4d0680629ad1cdf42811
Isaac <isaacloc@buffalo.edu>
2025-03-03 13:41:03 -0500

    Tweaks
    tweaks and cleans up any errors missed

--- Burger_Flipper_Code/Burger_Flipper_Code.ino
+++ Burger_Flipper_Code/Burger_Flipper_Code.ino
@@ -2,7 +2,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS 2; //uses pin 2 for onewire communication
+#define ONE_WIRE_BUS 2 //uses pin 2 for onewire communication
 
 OneWire oneWire(ONE_WIRE_BUS); //sets up onewire communcation
 DallasTemperature sensor(&oneWire); //sets up the DallasTemperature library to work with the OneWire bus
@@ -18,12 +18,12 @@ void setup() {
 }
 
 float getTemperature(){
-  sensor.requestTemperature(); //requests a temperature reading from the sensor
+  sensor.requestTemperatures(); //requests a temperature reading from the sensor
   float temperature = sensor.getTempFByIndex(0); //stores the temperature from the sensor
   return temperature; //returns the temperature value in degrees Fahrenheit
 }
 
-void ServoSpinner(int angle1, int angle2, Servo servo){
+void ServoSpinner(int angle1, int angle2, Servo &servo){
   servo.write(angle1); //moves the servo angle1 degrees
   delay(1000); //1 second delay
   servo.write(angle2); //moves the servo angle2 degrees  
