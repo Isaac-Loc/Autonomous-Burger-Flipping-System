1b31ab572b2834d513b2c63f5d0449dabb978bc6
erikaytmla <erika.yick24@tmla.org>
2025-03-08 21:22:16 -0500

    as
    aa

--- Burger_Flipper_Code/Burger_Flipper_Code.ino
+++ Burger_Flipper_Code/Burger_Flipper_Code.ino
@@ -6,34 +6,22 @@ Servo ProbeServo; //Probe Servo object
 //int laserReceivers[] {3,4,5}; //Pins 3, 4, and 5 are used for receivers
 //int laserPin 7;
 
-//DC Motor 1 Pins (Pins 8, 9, 10)
-#define ENA1 0
-#define IN1_1 1
-#define IN2_1 2
+//DC Motor 1 Pins (Pins 0, 1, 2)
+#define ENA1 0  // PWM pin to control motor speed (0-255)
+#define IN1_1 1 // Motor direction control pin 1 (Forward)
+#define IN2_1 2 // Motor direction control pin 2 (Backward)
+
+//DC Motor 2 Pins (Pins 3, 4, 5)
+#define ENA2 3  // PWM pin to control motor speed (0-255)
+#define IN1_2 4 // Motor direction control pin 1 (Forward)
+#define IN2_2 5 // Motor direction control pin 2 (Backward)
+
+//DC Motor 3 Pins (Pins 6, 7, 8)
+#define ENA3 6  // PWM pin to control motor speed (0-255)
+#define IN1_3 7 // Motor direction control pin 1 (Forward)
+#define IN2_3 8 // Motor direction control pin 2 (Backward)
 
 
-//#define ENA1 8  // PWM pin to control motor speed (0-255)
-//#define IN1_1 9 // Motor direction control pin 1 (Forward)
-//#define IN2_1 10 // Motor direction control pin 2 (Backward)
-
-//DC Motor 2 Pins (Pins 11, 12, 13)
-#define ENA2 3
-#define IN1_2 4
-#define IN2_2 5
-
-
-//#define ENA2 11 // PWM pin to control motor speed (0-255)
-//#define IN1_2 12 // Motor direction control pin 1 (Forward)
-//#define IN2_2 13 // Motor direction control pin 2 (Backward)
-
-//DC Motor 3 Pins (Pins 14, 15, 16)
-#define ENA3 6
-#define IN1_3 7
-#define IN2_3 8
-
-//#define ENA3 14 // PWM pin to control motor speed (0-255)
-//#define IN1_3 15 // Motor direction control pin 1 (Forward)
-//#define IN2_3 16 // Motor direction control pin 2 (Backward)
 
 
 void setup() {
@@ -156,3 +144,214 @@ void stopMotor(int in1Pin, int in2Pin) {
 /*Stop motor after moving
 
   StopMotor(in1Pin, in2Pin); */    
+
+int pinA = 11;int pinB = 7;int pinC = 4;int pinD = 2;
+int pinE = 1;int pinF = 10;int pinG = 5;
+int pinDP = 3;int D1 = 12;int D2 = 9;int D3 = 8; int D4 = 6;
+
+void setup() {//set all segments & digits as outputs
+pinMode(pinA, OUTPUT);
+pinMode(pinB, OUTPUT);
+pinMode(pinC, OUTPUT);
+pinMode(pinD, OUTPUT);
+pinMode(pinE, OUTPUT);
+pinMode(pinF, OUTPUT);
+pinMode(pinG, OUTPUT);
+pinMode(pinDP, OUTPUT);
+pinMode(D1, OUTPUT);
+pinMode(D2, OUTPUT);
+pinMode(D3, OUTPUT);
+pinMode(D4, OUTPUT);
+}
+
+void loop() {
+  
+  digit1();zero();delay(500);
+  digit2();one();delay(500);
+  digit3();two();delay(500);
+  digit4();three();delay(500);
+  digit3();four();delay(500);
+  digit2();five();delay(500);
+  digit1();six();delay(500);
+  digit2();seven();delay(500);
+  digit3();eight();delay(500);
+  digit4();nine();delay(500);
+ 
+  all4Digits();
+  allNumbers();
+
+}
+
+
+//functions representing numbers 0-9
+void zero(){
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, HIGH);
+  }
+
+  void one(){
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, HIGH);
+}
+
+void two(){
+  digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, LOW);
+  }
+  
+  void three(){
+    digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, LOW);
+    }
+    
+  void four(){
+    digitalWrite(pinA, HIGH);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+    }
+    
+  void five(){
+    digitalWrite(pinA, LOW);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+    }
+    
+  void six(){
+    digitalWrite(pinA, LOW);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+    }
+    
+  void seven(){
+    digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, HIGH);
+    }
+    
+  void eight(){
+    digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+    }
+    
+  void nine(){
+    digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+    }
+
+    void allNumbers(){
+one();delay(500);
+two();delay(500);
+three();delay(500);
+four();delay(500);
+five();delay(500);
+six();delay(500);
+seven();delay(500);
+eight();delay(500);
+nine();delay(500);
+      }
+
+  void turnOffAllSegments(){
+          digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+          }
+
+
+    //functions to select individual digits on the display
+    void digit1(){
+      digitalWrite(D1, HIGH);
+digitalWrite(D2, LOW);
+digitalWrite(D3, LOW);
+digitalWrite(D4, LOW);
+      }
+      
+    void digit2(){
+      digitalWrite(D1, LOW);
+digitalWrite(D2, HIGH);
+digitalWrite(D3, LOW);
+digitalWrite(D4, LOW);
+      }
+      
+    void digit3(){
+      digitalWrite(D1, LOW);
+digitalWrite(D2, LOW);
+digitalWrite(D3, HIGH);
+digitalWrite(D4, LOW);
+      }
+      
+    void digit4(){
+      digitalWrite(D1, LOW);
+digitalWrite(D2, LOW);
+digitalWrite(D3, LOW);
+digitalWrite(D4, HIGH);
+      }
+
+      void all4Digits(){
+        digitalWrite(D1, HIGH);
+digitalWrite(D2, HIGH);
+digitalWrite(D3, HIGH);
+digitalWrite(D4, HIGH);
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
