e5d2a0586128f947fc0af384d5fedf1fa8cc0a29
aasami27 <sami.official554@gmail.com>
2025-04-11 23:52:14 -0400

    Update Burger_Flipper_Code.ino

--- Burger_Flipper_Code/Burger_Flipper_Code.ino
+++ Burger_Flipper_Code/Burger_Flipper_Code.ino
@@ -1,93 +1,157 @@
+#include <DallasTemperature.h>
+#include <OneWire.h>
 #include <Servo.h>
-//#include <OneWire.h>
-//#include <DallasTemperature.h>
-
-//OneWire oneWire(ONE_WIRE_BUS); //sets up onewire communcation
-//DallasTemperature sensor(&oneWire); //sets up the DallasTemperature library to work with the OneWire bus
 
 Servo SpatulaServo; //Spatula Servo object
-Servo ProbeServo; //Probe Servo object
 
-/*#define ONE_WIRE_BUS 2 //uses pin 2 for onewire communication
-int laserReceivers[] {3,4,5}; //Pins 3, 4, and 5 are used for receivers
+//DC Motor 1 Pins (Pins 10, 13, 2) Probe DC y axis
+#define ENA1 10  // PWM pin to control motor speed (0-255)
+#define IN1_1 13 // Motor direction control pin 1 (Forward)
+#define IN1_2 2 // Motor direction control pin 2 (Backward)
 
-//DC Motor 1 Pins (Pins 8, 9, 10)
-#define ENA1 8  // PWM pin to control motor speed (0-255)
-#define IN1_1 9 // Motor direction control pin 1 (Forward)
-#define IN2_1 10 // Motor direction control pin 2 (Backward)
+//DC Motor 2 Pins (Pins 3, 4, 5) Spatula DC
+#define ENA2 3  // PWM pin to control motor speed (0-255)
+#define IN2_1 4 // Motor direction control pin 1 (Forward)
+#define IN2_2 5 // Motor direction control pin 2 (Backward)
 
-//DC Motor 2 Pins (Pins 11, 12, 13)
-#define ENA2 11 // PWM pin to control motor speed (0-255)
-#define IN1_2 12 // Motor direction control pin 1 (Forward)
-#define IN2_2 13 // Motor direction control pin 2 (Backward)
+//DC Motor 3 Pins (Pins 6, 7, 8) Probe DC x axis
+#define ENA3 6  // PWM pin to control motor speed (0-255)
+#define IN3_1 7 // Motor direction control pin 1 (Forward)
+#define IN3_2 8 // Motor direction control pin 2 (Backward)
 
-//DC Motor 3 Pins (Pins 14, 15, 16)
-#define ENA3 14 // PWM pin to control motor speed (0-255)
-#define IN1_3 15 // Motor direction control pin 1 (Forward)
-#define IN2_3 16 // Motor direction control pin 2 (Backward)*/
+//ALL LASERS AND RECEIVER PINS
+#define laser1 14 //Pin for laser 1
+#define laser2 15 //Pin for laser 2
+#define laser3 16 //Pin for laser 3
+#define laser4 17 //Pin for laser 4
+#define laser5 52 //Pin for laser 5
+#define laser6 53 //Pin for laser 6
+
+#define receiver1 18 //Pin for receiver 1
+#define receiver2 19 //Pin for receiver 2
+#define receiver3 20 //Pin for receiver 3
+#define receiver4 21 //Pin for receiver 4
+#define receiver5 40 //Pin for receiver 5
+#define receiver6 41 //Pin for receiver 6
+
+#define ONE_WIRE_BUS 22 //Pin for temperature probe
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+const int KillSwitch = 23; //Pin for kill switch
+
+// Define segment pins for 3 digit display
+const int segmentPins[8] = {24, 25, 26, 27, 28, 29, 30, 31}; // A to DP
+const int digitPins[3] = {32, 33, 34}; // Common cathode pins
+
+
+// Define digit representation for numbers 0-9
+const byte digits[10] = {
+  0b00111111, // 0
+  0b00000110, // 1
+  0b01011011, // 2
+  0b01001111, // 3
+  0b01100110, // 4
+  0b01101101, // 5
+  0b01111101, // 6
+  0b00000111, // 7
+  0b01111111, // 8
+  0b01101111  // 9
+};
 
 
 void setup() {
   // put your setup code here, to run once:
-  //sensor.begin(); //initializes the sensor for arduino communication
-  SpatulaServo.attach(6); //attach spatula servo to pin 6
-
-  /*for(int i=0; i< 3; i++){
-    pinMode(laserReceivers[i], INPUT); //sets each receiver pin as an input
-  }
+  SpatulaServo.attach(9); //attach spatula servo to pin 6
 
    // Initialize motor pins
   pinMode(ENA1, OUTPUT);  // Motor 1 speed control
   pinMode(IN1_1, OUTPUT); // Motor 1 direction control (Forward)
-  pinMode(IN2_1, OUTPUT); // Motor 1 direction control (Backward)
-  
+  pinMode(IN1_2, OUTPUT); // Motor 1 direction control (Backward)
+ 
   pinMode(ENA2, OUTPUT);  // Motor 2 speed control
-  pinMode(IN1_2, OUTPUT); // Motor 2 direction control (Forward)
+  pinMode(IN2_1, OUTPUT); // Motor 2 direction control (Forward)
   pinMode(IN2_2, OUTPUT); // Motor 2 direction control (Backward)
 
   pinMode(ENA3, OUTPUT);  // Motor 3 speed control
-  pinMode(IN1_3, OUTPUT); // Motor 3 direction control (Forward)
-  pinMode(IN2_3, OUTPUT); // Motor 3 direction control (Backward)*/
+  pinMode(IN3_1, OUTPUT); // Motor 3 direction control (Forward)
+  pinMode(IN3_2, OUTPUT); // Motor 3 direction control (Backward)
+
+  pinMode(laser1, OUTPUT); // set the laser pin to output mode
+  pinMode(receiver1, INPUT); // set the laser pin to output mode
+  digitalWrite(laser1, HIGH); // emit red laser
+
+  pinMode(laser2, OUTPUT); // set the laser pin to output mode
+  pinMode(receiver2, INPUT); // set the laser pin to output mode
+  digitalWrite(laser2, HIGH);// emit red laser
+
+  pinMode(laser3, OUTPUT); // set the laser pin to output mode
+  pinMode(receiver3, INPUT); // set the laser pin to output mode
+  digitalWrite(laser3, HIGH);// emit red laser
+
+  pinMode(laser4, OUTPUT); // set the laser pin to output mode
+  pinMode(receiver4, INPUT); // set the laser pin to output mode
+  digitalWrite(laser4, HIGH); // emit red laser
+
+  pinMode(laser5, OUTPUT);
+  pinMode(receiver5, INPUT);
+  digitalWrite(laser5, HIGH); // emit red laser
 
 
+
+  pinMode(laser6, OUTPUT);
+  pinMode(receiver6, INPUT); 
+  digitalWrite(laser6, HIGH); // emit red laser
+
+
+
+  // Set segment pins as OUTPUT
+  for (int i = 0; i < 8; i++) {
+    pinMode(segmentPins[i], OUTPUT);
+  }
+ 
+  // Set digit control pins as OUTPUT
+  for (int i = 0; i < 3; i++) {
+    pinMode(digitPins[i], OUTPUT);
+    digitalWrite(digitPins[i], HIGH); // Start with all digits off
+  }
+
+  pinMode(KillSwitch,INPUT_PULLUP); // sets up kill switch pin
+ 
+  Serial.begin(9600);
+  sensors.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  ServoSpinner(180,0,SpatulaServo);
+ // CheckKillSwitch();
+  //1 is not connected 0 is connected to receiver
+  int value1 = digitalRead(receiver1); // receiver/detector send either LOW or HIGH (no analog values!)
+  int value2 = digitalRead(receiver2); // receiver/detector send either LOW or HIGH (no analog values!)
+  int value3 = digitalRead(receiver3); // receiver/detector send either LOW or HIGH (no analog values!)
+  int value4 = digitalRead(receiver4); // receiver/detector send either LOW or HIGH (no analog values!)
+  int value5 = digitalRead(receiver5);
+  int value6 = digitalRead(receiver6);
+
+  Serial.print(value1);
+  Serial.print(value2);
+  Serial.print(value3);
+  Serial.print(value4);
+  Serial.print(value5);
+  Serial.print(value6);
+
 }
-
-
-
-
 
 
 // GENERAL HELPER FUNCTIONS
 
-
-//TEMPERATURE PROBE FUNCTIONALITY
-
-
-/*float getTemperature(){
-  sensor.requestTemperatures(); //requests a temperature reading from the sensor
-  float temperature = sensor.getTempFByIndex(0); //stores the temperature from the sensor
-  return temperature; //returns the temperature value in degrees Fahrenheit
+//Temperature probe
+float GetTemperature(){
+  sensors.requestTemperatures();
+  float Celsius = sensors.getTempCByIndex(0);
+  float Fahrenheit = sensors.toFahrenheit(Celsius);
+  return Fahrenheit;
 }
-
-//LASER RECEIVER CHECKER FUNCTIONALITY
-
-
-void checkLaserReceivers() {
-  for (int i = 0; i < 3; i++) {
-    int laserStatus = digitalRead(laserReceivers[i]);
-    if (laserStatus == LOW) { // Laser is blocked
-      Serial.print("Laser Receiver ");
-      Serial.print(i + 1);  // Increment index to start at 1
-      Serial.println(" has lost connection!");
-    }
-  }
-}
-*/
 
 //SERVO MOTORS FUNCTIONALITY
 
@@ -98,8 +162,6 @@ void ServoSpinner(int angle1, int angle2, Servo &servo){
   servo.write(angle2); //moves the servo angle2 degrees  
   delay(1000); //1 second delay
 }
-
-
 
 //DC MOTOR FUNCTIONALITY
 
@@ -114,11 +176,10 @@ void moveForward(int enaPin, int in1Pin, int in2Pin, int speed, int duration) {
   stopMotor(in1Pin, in2Pin);     // Stop motor after moving
 }
 
-//How to Call the above function: 
+//How to Call the above function:
 /* Move motor 1 forward with speed 200 for 5 seconds
 
   moveForward(ENA1, IN1_1, IN2_1, 200, 5000); */
-
 
 
 // Function to move the motor backward
@@ -150,8 +211,43 @@ void stopMotor(int in1Pin, int in2Pin) {
   StopMotor(in1Pin, in2Pin); */    
 
 
+//Function for number shown on the 3 digit display
 
+void displayNumber(int number) {
+  int digitValues[3] = {number / 100, (number / 10) % 10, number % 10};
 
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(digitPins[i], LOW); // Enable digit
 
+    showSegments(digits[digitValues[i]]); // Show corresponding number
+   
+    delay(5); // Small delay for persistence of vision
+   
+    digitalWrite(digitPins[i], HIGH); // Disable digit
+  }
+}
 
+//Helper function for display numbers
 
+void showSegments(byte segments) {
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(segmentPins[i], (segments >> i) & 1);
+  }
+}
+
+//Kill switch function
+
+void CheckKillSwitch(){
+  if(digitalRead(KillSwitch)==LOW){
+    digitalWrite(laser1, LOW); // emit red laser
+    digitalWrite(laser2, LOW); // emit red laser
+    digitalWrite(laser3, LOW); // emit red laser
+    digitalWrite(laser4, LOW); // emit red laser
+    stopMotor(IN1_1,IN1_2);
+    stopMotor(IN2_1,IN2_2);
+    stopMotor(IN3_1,IN3_2);
+    while(true){
+      delay(30303033030303030);
+    }
+  }
+}
